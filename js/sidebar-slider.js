3c0094f932ba94c79fa60898ff25e2a4c8060622
shpp-vprihozhenko <vprihozhenko@gmail.com>
2016-03-16 10:06:07 +0200

    slidebar animation 2

--- js/sidebar-slider.js
+++ js/sidebar-slider.js
@@ -10,24 +10,24 @@ function SidebarSlider(initialTab) {
 SidebarSlider.prototype.showTab = function(tab) {
 	console.log(tab, this.active);
 	if (tab != this.active) {
+
 		if (this.active) {
-			this.active.css('left', this.width);
-		} else {
-			this.width = tab.width();
+			$('.tab').css({
+				'opacity': 0,
+				'z-index': 1,
+				'left': this.width
+			});
 		}
 		this.active = tab;
-		tab.css('left', 0);
+		tab.css({
+		 	'opacity': 1,
+			'z-index': 2,
+			'left': 0
+		});
+
 		/*
 		$('.tab').hide( 'slide', {}, 1000 );
 		tab.show('slide',1000);
-		$('.tab').css({
-			'opacity': 0,
-			'z-index': 1
-		});
-		tab.css({
-			'opacity': 1,
-			'z-index': 2
-		});
 		this.active = tab;
 		*/
 	}
