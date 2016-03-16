3621aba78213d1d3f30c0d90a94648bb51fcf704
shpp-vprihozhenko <vprihozhenko@gmail.com>
2016-03-16 09:19:54 +0200

    slidebar animation

--- js/sidebar-slider.js
+++ js/sidebar-slider.js
@@ -2,13 +2,25 @@
 function SidebarSlider(initialTab) {
 	this.active = null;
 	this.showTab(initialTab);
+	if (this.width==undefined){
+		this.width = initialTab.width();
+	}
 }
 
 SidebarSlider.prototype.showTab = function(tab) {
 	console.log(tab, this.active);
 	if (tab != this.active) {
+		if (this.active) {
+			this.active.css('left', this.width);
+		} else {
+			this.width = tab.width();
+		}
+		this.active = tab;
+		tab.css('left', 0);
+		/*
 		$('.tab').hide( 'slide', {}, 1000 );
 		tab.show('slide',1000);
 		this.active = tab;
+		*/
 	}
 };
